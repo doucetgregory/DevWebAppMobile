de7068b5711df2e9c9337012d8a05535ab6242dd
Grégory Doucet <doucetgregoryperso@outlook.fr>
2019-02-26 11:10:43 +0100

    Ajout de classe Firebase

---
+++ src/class/Firebase.js
@@ -0,0 +1,28 @@
+import * as firebase from 'firebase';
+
+export default class Firebase {
+    constructor(firebase) {
+        var config = {
+            apiKey: "AIzaSyDn3Kg6ZbE3ozO4JvqW38reLIv79CkYxUo",
+            authDomain: "devwebapplimobile.firebaseapp.com",
+            databaseURL: "https://devwebapplimobile.firebaseio.com",
+            projectId: "devwebapplimobile",
+            storageBucket: "devwebapplimobile.appspot.com",
+            messagingSenderId: "624557437842"
+        };
+
+        firebase.initializeApp(config);
+        this.db = firebase.firestore();
+    }
+
+
+    addUser(data, callback) {
+        return new Promise((resolve, reject)=>{
+            this.db.collection("users").add(data).then(newData=>{
+                console.log("Document written with ID : ", newData.id);
+                data.id = newData.id;
+                resolve(data);
+            })
+        })
+    }
+}
