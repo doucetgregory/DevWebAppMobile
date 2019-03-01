595a25e0f2587d18be87935989a25a2dbc5a5e73
Alban GUILLET <albanguillet98@gmail.com>
2019-03-01 10:03:50 +0100

    implementing Firebase connection

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("hello-world !");
+import * as firebase from 'firebase/app';
+import Player from './class/Player.js';
+import Monster from './class/Monster.js';
+import FirebaseService from './class/Firebase.js';
+
+var Alban = new Player("Alban");
+console.log(Alban);
+
+var monster = new Monster("Monstre");
+console.log(monster);
+
+Alban.attack(monster);
+console.log(monster);
+
+var firebaseService = new FirebaseService(firebase)
+firebaseService.addUser(Alban);
